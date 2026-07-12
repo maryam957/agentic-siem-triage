97df0c198fd1ed1bd7067516fbc495f86b379df1
noor <nooramir0102@gmail.com>
2026-07-13 00:07:21 +0500

    Added project entry files

---
+++ agents/graph.py
@@ -0,0 +1,10 @@
+"""
+LangGraph workflow skeleton.
+
+The workflow implementation will be added in later milestones.
+"""
+
+
+def build_graph():
+    """Build and return the LangGraph workflow."""
+    pass
---
+++ agents/nodes.py
@@ -0,0 +1,19 @@
+"""
+Node function stubs for the LangGraph workflow.
+"""
+
+
+def ingest_node(state):
+    pass
+
+
+def enrich_node(state):
+    pass
+
+
+def analyze_node(state):
+    pass
+
+
+def report_node(state):
+    pass
